745dd468269ccf246889466dddbbf67d23391720
Glitch (videogamefilter) <none>
2022-05-24 22:23:19 +0000

    ✔️🤗 Updated with Glitch

--- script.js
+++ script.js
@@ -7,22 +7,36 @@ function loadInitWebpage() {
       return response.json();
     })
     .then(function(json) {
-      let card = document.createElement("div");
-      card.setAttribute("class","gameCards");
+      
       let data = json.results
-      console.log(data.length);
+      console.log(data);
       for (let index = 0; index < data.length; index ++) {
-        loadGameCard(card, data[index]["name"], data[index]["rating"], data[index]["released"],data[index]["background_image"])  
+        loadGameCard(data[index]["name"], data[index]["rating"], data[index]["released"],data[index]["background_image"])  
       }
-      $(".videoGamesContainer").append(card);
+      
     })
     // $(".videoGamesContainer").
 }
-function loadGameCard(cardDiv, name, rating, releaseDate,bgImg) {
-  
-  $(".gameCards").append($(`<div class = "gameImgContainer"><img class = "gameImages" src = ${bgImg}></div>`));
-  $(".gameCards").append($(`<div><p>${name} has a rating of ${rating}<br>Release Date: ${releaseDate}</p></div>`));
-  
+function loadGameCard(name, rating, releaseDate,bgImg) {
+  let cardDiv = `<div class = "gameCards"><div class = "gameImgContainer"><img class = "gameImages" src = ${bgImg}></div><div><p>${name} has a rating of ${rating}<br>Release Date: ${releaseDate}</p></div></div>`;
+  $(".videoGamesContainer").append(cardDiv);
 }
+
+$(".search").click(function() {
+  $(".videoGamesContainer").empty();
+  let input = $("input").val();
+  let newAPICall = apiURL + "&search=" + input;
+  fetch(newAPICall)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    let data = json.results;
+    console.log(data);
+    for (let ind = 0; ind < data.length; ind ++) {
+      loadGameCard(data[ind]["name"],data[ind]["rating"],data[ind]["released"],data[ind]["background_image"]);
+    }
+  })
+})
 
 loadInitWebpage();
